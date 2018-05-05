f74d786c3b0b8b361eaddc2bae5adff35bdebcae
Ivan <johnmoony@mail.ru>
2018-05-05 22:12:58 +0300

    Arango init

--- init.arango.js
+++ init.arango.js
@@ -1,162 +1,25 @@
-/*
-
-Модель метаграфа в аранго
-// node 1
-{
-	name: "meta_parent_1",
-
-}
-
-
-*/
-
-
-
+// Init graph db
+db._createDatabase("mgraph");
 db._useDatabase("mgraph");
 
-db.Nodes.truncate();
-db.NodesConnections.truncate();
+db._create('Nodes');
+db._create('NodesConnections');
 
-tr = function() {
-	db.Nodes.truncate();
-	db.NodesConnections.truncate();
-}
-
-db._query(`
-	for c in Nodes return c
-`);
-
-db._query(`
-	for c in NodesConnections return c
-`);
-
-
-// NODES
-db._query(`
-	let data = [
-	{_key: "1", name: "Berlin"}, 
-	{_key: "2", name: "London"},
-	{_key: "3", name: "DC Columbia", sub_cities: [{name: "Washington"}]}
-	]
-	for d in data insert d into Nodes
-	
-`);
-
-// CONNECTIONS
-db._query(`
-	insert { _from: "Nodes/1", _to: "Nodes/2" } into NodesConnections	
-`);
-
-
-db._query(`
-	FOR e, v, p IN ANY 'NodesConnections'
-`);
-
-
-
-// Create named graph
 var graph_module = require("org/arangodb/general-graph");
 var edgeDefinitions = [{
-	collection: "NodesConnections", from: [ "Nodes" ], to : [ "Nodes" ]
+    collection: "NodesConnections", from: [ "Nodes" ], to : [ "Nodes" ]
 }];
 graph = graph_module._create("NodesGraph", edgeDefinitions);
 
-// Simple query over named graph
-db._query(`
-	FOR v IN 1..1 OUTBOUND "Nodes/1" GRAPH "NodesGraph"
-	RETURN v.name
-`);
-
-// what for? graph._addVertexCollection('Nodes');
+db.Nodes.ensureIndex({ type: "hash", fields: [ "from" ], sparse: true });
+db.Nodes.ensureIndex({ type: "hash", fields: [ "to" ], sparse: true });
 
 
-// for c in Nodes return c
-// for c in NodesConnections return c
+// Init doc db
+db._createDatabase("mgraph-doc");
+db._useDatabase("mgraph-doc");
 
+db._create('Nodes');
 
-// TOWNS CSV TEST
-// arangoimp --file data/t100k/town_with_types.arango.csv --separator ';' --type csv --collection towns --create-collection true
-arangoimp --file data/t1M/town_with_types.arango.csv --separator ';' --type csv --collection towns --create-collection true
-
-
-db._useDatabase("_system");
-db.towns.ensureIndex({ type: "skiplist", fields: [ "type" ], sparse: false }); 
-db.towns.ensureIndex({ type: "skiplist", fields: [ "name" ], sparse: false }); 
-db.towns.ensureIndex({ type: "skiplist", fields: [ "nid" ], sparse: false }); 
-
-db.towns.truncate();
-
-
-
-// Метаграфы через граф
-// Базовый конфиг - две связанные вершины внутри метавершины.
-  // Вершины
-  db._query(`
-  	let data = [
-	{
-		_key: "1",
-		name: "v1",
-		type: "vertex"
-	},
-	{
-		_key: "2",
-		name: "v2",
-		type: "vertex"
-	},
-	{
-		_key: "3",
-		name: "mv1",
-		type: "metavertex"
-	},
-	{
-		_key: "4",
-		name: "e1",
-		type: "edge"
-	}]
-	for d in data insert d into Nodes 
- `);
-
-  // Связи
-    db._query(`
-  	let data = [
-	{
-		"_from":"Nodes/1",
-		"_to":"Nodes/4"
-	},
-	{
-		"_from":"Nodes/4",
-		"_to":"Nodes/2"
-	},
-	{
-		"_from":"Nodes/4",
-		"_to":"Nodes/3"
-	},
-	{
-		"_from":"Nodes/2",
-		"_to":"Nodes/3"
-	},
-	{
-		"_from":"Nodes/1",
-		"_to":"Nodes/3"
-	}
-	]
-	for d in data insert d into NodesConnections
-  `);
-
-
-// Init for tests
-arangoimp --file nodes.csv --type csv --separator ';' --collection Nodes --server.database mgraph --server.password 1234
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+db.Nodes.ensureIndex({ type: "hash", fields: [ "from" ], sparse: true });
+db.Nodes.ensureIndex({ type: "hash", fields: [ "to" ], sparse: true });
